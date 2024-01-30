6ae48b51cbea9d0b458e2b82d6b539b9abd83237
Arthaud <arthaudp33@gmail.com>
2024-01-30 17:28:43 +0100

    package: bundle with gulp

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+import { deleteSync } from "del";
+import gulp from "gulp";
+import zip from "gulp-zip";
+
+gulp.task("copyBundleFiles", () => {
+  return gulp.src(".bundle_files/**/*").pipe(gulp.dest("./bundle/"));
+});
+
+gulp.task("copyDistInBundle", () => {
+  return gulp.src("dist/**/*").pipe(gulp.dest("./bundle/dist"));
+});
+
+gulp.task("zipBundle", () => {
+  return gulp
+    .src("./bundle/**/*")
+    .pipe(zip("bundle.zip"))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("cleanBundle", async () => {
+  return deleteSync("bundle");
+});
+
+gulp.task(
+  "bundle",
+  gulp.series(
+    "copyBundleFiles",
+    "copyDistInBundle",
+    "zipBundle",
+    "cleanBundle",
+  ),
+);
