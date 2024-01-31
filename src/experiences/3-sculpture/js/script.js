52edefe486def4778cd1e9ee4a2f772f7a10a34f
Lucas froidefond <lucas.froidefond@mmibordeaux.com>
2024-01-31 18:43:38 +0100

    wip : initialization

--- src/experiences/3-sculpture/js/script.js
+++ src/experiences/3-sculpture/js/script.js
@@ -1,39 +1,108 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-// Canvas
-const canvas = document.querySelector("canvas.webgl");
-
-// Scene
-const scene = new THREE.Scene();
-
-/**
- * Object
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-/**
- * Sizes
- */
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
-/**
- * Camera
- */
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-scene.add(camera);
+//
+// INITIALIZATION
+//
 
-/**
- * Renderer
- */
+const canvas = document.querySelector("canvas.webgl");
+
+//Renderer
+
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  antialias: true,
 });
+renderer.toneMapping = THREE.CineonToneMapping;
+renderer.toneMappingExposure = 1.75;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.setClearColor("#211d20");
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Scene
+
+const scene = new THREE.Scene();
+
+//Camera
+
+const camera = new THREE.PerspectiveCamera(
+  35,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(5, 5, 5);
+scene.add(camera);
+
+//
+// OBJET
+//
+
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "blue" });
+const cube = new THREE.Mesh(cubeGeometry, material);
+
+cube.position.set(0, 0, 0);
+
+scene.add(cube);
+
+//
+// EVENT
+//
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+let touchBefore = 0;
+let currentTouch = 0;
+
+window.addEventListener("touchmove", (event) => {
+  currentTouch = event.touches[0].clientX;
+});
+
+//
+// ANIMATE
+//
+
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+const tick = () => {
+  //Time
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+
+  //Animate in tick
+  camera.lookAt(cube.position);
+
+  const touchX = currentTouch - touchBefore;
+
+  touchBefore = currentTouch;
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
