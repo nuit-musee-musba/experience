add5afb6598f54d3789ae447401f33467d46eb7f
Auréliane DOR <aureliane.dor@gmail.com>
2024-02-01 12:41:40 +0100

    remove 3D

--- src/experiences/2-arts-graphiques/ts/script.ts
+++ src/experiences/2-arts-graphiques/ts/script.ts
@@ -1,14 +1,17 @@
-import Experience from "./Experience/Experience";
+// import Experience from "./Experience/Experience";
+//
+// const canvas = document.querySelector<HTMLCanvasElement>("canvas.webgl");
+//
+// if (!canvas) {
+//   console.error(
+//     "Canvas with a class of 'webgl' is required in order for the experience to work"
+//   );
+//   throw new Error(
+//     "Canvas with a class of 'webgl' is required in order for the experience to work"
+//   );
+// }
+//
+// const experience = new Experience(canvas);
 
-const canvas = document.querySelector<HTMLCanvasElement>("canvas.webgl");
 
-if (!canvas) {
-  console.error(
-    "Canvas with a class of 'webgl' is required in order for the experience to work"
-  );
-  throw new Error(
-    "Canvas with a class of 'webgl' is required in order for the experience to work"
-  );
-}
-
-const experience = new Experience(canvas);
+console.log('no more 3D');
