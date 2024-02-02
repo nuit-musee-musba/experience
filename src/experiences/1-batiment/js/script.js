0efce170ac53e039cd6610d9f8ef2e2e4848887a
Emma Guillaume <emma.guillaume@mmibordeaux.com>
2024-02-02 15:33:55 +0100

    [CLEAN]

--- src/experiences/1-batiment/js/script.js
+++ src/experiences/1-batiment/js/script.js
@@ -4,60 +4,58 @@ import gsap from "gsap";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { period } from "./period";
 
+let index = 0;
+let previousTime = 0;
+let time = 0;
+const clock = new THREE.Clock();
 const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
+const gltfLoader = new GLTFLoader();
+let mixer = null;
+
+//MATERIALS
+
+gltfLoader.load("./assets/ANCIEN_MUSEE.glb", (gltf) => {
+  gltf.scene.traverse((child) => {
+    if (child.isMesh) {
+      // child.material = new THREE.MeshBasicMaterial({ color: 'red', wireframe: true });
+    }
+  });
+  scene.add(gltf.scene);
+  gltf.scene.rotation.y = -1.25;
+});
+
+// LIGHTS
 
 const ambientLight = new THREE.AmbientLight("#ffffff", 0.8);
 
 const directionalLight = new THREE.DirectionalLight("#EBF5F6", 3);
-// const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight)
 directionalLight.position.set(1, 3, 4);
 directionalLight.lookAt(4, 2, 4);
 
 scene.add(ambientLight, directionalLight);
 
-// GUI
-// gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
-
-const gltfLoader = new GLTFLoader();
-
-let mixer = null;
-
-gltfLoader.load("./assets/blockout.glb", (gltf) => {
-  scene.add(gltf.scene);
-
-  mixer = new THREE.AnimationMixer(gltf.scene);
-
-  gltf.animations.sort((a, b) => a.timestamp - b.timestamp);
-
-  gltf.animations.forEach((animation, index) => {
-    const action = mixer.clipAction(animation);
-
-    const delay =
-      animation.timestamp !== undefined ? animation.timestamp : index * 1000;
-
-    action.setEffectiveTimeScale(1);
-    action.setEffectiveWeight(1);
-    action.clampWhenFinished = true;
-    action.loop = THREE.LoopOnce;
-    action.startAt(delay / 1000);
-    action.play();
-  });
-});
-
-//MATERIALS
-
-// gltfLoader.load("./assets/ANCIEN_MUSEE.glb", (gltf) => {
-//   gltf.scene.traverse((child) => {
-//     if (child.isMesh) {
-//       // child.material = new THREE.MeshBasicMaterial({ color: 'red', wireframe: true });
-//     }
-//   });
+// gltfLoader.load("./assets/blockout.glb", (gltf) => {
 //   scene.add(gltf.scene);
-//   gltf.scene.rotation.y = -1.25;
-// });
 
-// const periodCameraPosition = period.map((step) => {return step.position});
+//   mixer = new THREE.AnimationMixer(gltf.scene);
+
+//   gltf.animations.sort((a, b) => a.timestamp - b.timestamp);
+
+//   gltf.animations.forEach((animation, index) => {
+//     const action = mixer.clipAction(animation);
+
+//     const delay =
+//       animation.timestamp !== undefined ? animation.timestamp : index * 1000;
+
+//     action.setEffectiveTimeScale(1);
+//     action.setEffectiveWeight(1);
+//     action.clampWhenFinished = true;
+//     action.loop = THREE.LoopOnce;
+//     action.startAt(delay / 1000);
+//     action.play();
+//   });
+// });
 
 // SIZES
 const sizes = {
@@ -93,16 +91,40 @@ const getCameraPositionForTarget = (position) => {
   return { x: position.x + 0, y: position.y + 1, z: position.z + 1 };
 };
 
-let index = 0;
+//RENDERER
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.setClearColor("#808E90");
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+  const deltaTime = elapsedTime - previousTime;
+  previousTime = elapsedTime;
+  time += 0.1;
+
+  if (mixer) {
+    mixer.update(deltaTime);
+  }
+
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
+
 const endMenu = document.getElementById("end-menu");
+document.getElementById("restart-button").addEventListener("click", restart);
+document.getElementById("nextButton").addEventListener("click", nextStep);
 
 const restart = () => {
   index = 0;
   handleFocusPeriod(period[index]);
   endMenu.style.display = "none";
 };
-
-document.getElementById("restart-button").addEventListener("click", restart);
 
 const nextStep = () => {
   if (index + 1 < period.length) {
@@ -113,8 +135,6 @@ const nextStep = () => {
     endMenu.style.display = "flex";
   }
 };
-
-document.getElementById("nextButton").addEventListener("click", nextStep);
 
 for (let i = 1; i <= 4; i++) {
   document.getElementById(`period${i}`).addEventListener("click", () => {
@@ -152,32 +172,3 @@ function handleFocusPeriod(step) {
   });
 }
 handleFocusPeriod(period[index]);
-
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.setClearColor("#808E90");
-
-let previousTime = 0;
-
-let time = 0;
-const clock = new THREE.Clock();
-
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - previousTime;
-  previousTime = elapsedTime;
-  time += 0.1;
-
-  if (mixer) {
-    mixer.update(deltaTime);
-  }
-
-  controls.update();
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(tick);
-};
-
-tick();
