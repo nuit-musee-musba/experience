8006823bf96bf67fa1e9c07b8621b0848cf110b2
EmmaGuillaume <emma.guillaume@mmibordeaux.com>
2024-02-05 11:27:35 +0100

    [FEAT] - add interest point clickable with raycaster

--- src/experiences/1-batiment/js/period.js
+++ src/experiences/1-batiment/js/period.js
@@ -1,7 +1,44 @@
 export const period = [
-    {title : 'La Création du Musée', date: '1801', position: {x: 0, y: 0, z: 2}, description:[ "Le musée des Beaux-Arts de Bordeaux est fondé en 1801, devenant le premier musée de la ville.", "La Galerie des Beaux-Arts, espace d'exposition temporaire, est intégrée de part et d'autre du jardin du palais Rohan et du cours d'Albret.", "Malgré l’aménagement conçu par l’ingénieur de la Ville Richard Bonfin en 1809, la galerie, ouverte le 1er octobre 1810, ne put présenter toutes ses œuvres."]},
-    {title : 'Déplacements Successifs et Incendies', date: '1809-1870', position: {x: 4, y: 1, z: 2}, description: [ "Malgré des ajustements en 1809, les déménagements se succèdent, avec des tentatives d'installation dans divers endroits.", "Incendies en 1862 et 1870 obligent à des travaux de réfection, déplaçant temporairement la collection dans des structures vulnérables."]},
-    {title : 'Construction du Musée Actuel', date: '1875', position: {x: 9, y: 2, z: 9}, description: [ "Le musée des Beaux-Arts de Bordeaux est fondé en 1801, devenant le premier musée de la ville.", "La Galerie des Beaux-Arts, espace d'exposition temporaire, est intégrée de part et d'autre du jardin du palais Rohan et du cours d'Albret."]},
-    {title : "Aujourd'hui", date: '', position: {x: 9, y: 5, z: 9}, description: [ "Le musée des Beaux-Arts de Bordeaux continue de jouer un rôle majeur dans la vie culturelle de la ville, présentant des collections permanentes dans les ailes construites par Burguet.", "La Galerie des Beaux-Arts continue d'accueillir des expositions temporaires, maintenant conformes aux normes de sécurité et d'accessibilité contemporaines."]},
-    // {title : 'Lorem Ipsum5', date: '1963', position: {x: 4, y: 3, z: 2}, description: [ "Le musée des Beaux-Arts de Bordeaux est fondé en 1801, devenant le premier musée de la ville.", "La Galerie des Beaux-Arts, espace d'exposition temporaire, est intégrée de part et d'autre du jardin du palais Rohan et du cours d'Albret."]}
-]
+  {
+    title: "La Création du Musée",
+    date: "1801",
+    position: { x: 0, y: 0, z: 1 },
+    description: [
+      "Le musée des Beaux-Arts de Bordeaux est fondé en 1801, devenant le premier musée de la ville.",
+      "La Galerie des Beaux-Arts, espace d'exposition temporaire, est intégrée de part et d'autre du jardin du palais Rohan et du cours d'Albret.",
+      "Malgré l’aménagement conçu par l’ingénieur de la Ville Richard Bonfin en 1809, la galerie, ouverte le 1er octobre 1810, ne put présenter toutes ses œuvres.",
+    ],
+    cubePosition: { x: 0, y: 0, z: 2 },
+  },
+  {
+    title: "Déplacements Successifs et Incendies",
+    date: "1809-1870",
+    position: { x: 4, y: 1, z: 3 },
+    description: [
+      "Malgré des ajustements en 1809, les déménagements se succèdent, avec des tentatives d'installation dans divers endroits.",
+      "Incendies en 1862 et 1870 obligent à des travaux de réfection, déplaçant temporairement la collection dans des structures vulnérables.",
+    ],
+    cubePosition: { x: 2, y: 0, z: 3 },
+  },
+  {
+    title: "Construction du Musée Actuel",
+    date: "1875",
+    position: { x: 9, y: 2, z: 9 },
+    description: [
+      "Le musée des Beaux-Arts de Bordeaux est fondé en 1801, devenant le premier musée de la ville.",
+      "La Galerie des Beaux-Arts, espace d'exposition temporaire, est intégrée de part et d'autre du jardin du palais Rohan et du cours d'Albret.",
+    ],
+    cubePosition: { x: 3, y: 0, z: 3 },
+  },
+  {
+    title: "Aujourd'hui",
+    date: "",
+    position: { x: 9, y: 5, z: 9 },
+    description: [
+      "Le musée des Beaux-Arts de Bordeaux continue de jouer un rôle majeur dans la vie culturelle de la ville, présentant des collections permanentes dans les ailes construites par Burguet.",
+      "La Galerie des Beaux-Arts continue d'accueillir des expositions temporaires, maintenant conformes aux normes de sécurité et d'accessibilité contemporaines.",
+    ],
+    cubePosition: { x: 3, y: 0, z: 4 },
+  },
+  // {title : 'Lorem Ipsum5', date: '1963', position: {x: 4, y: 3, z: 2}, description: [ "Le musée des Beaux-Arts de Bordeaux est fondé en 1801, devenant le premier musée de la ville.", "La Galerie des Beaux-Arts, espace d'exposition temporaire, est intégrée de part et d'autre du jardin du palais Rohan et du cours d'Albret."]}
+];
