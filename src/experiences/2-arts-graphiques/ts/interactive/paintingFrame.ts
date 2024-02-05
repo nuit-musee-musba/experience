d2ee27722d0736081095dc4a43848bf71af97177
Auréliane DOR <aureliane.dor@gmail.com>
2024-02-05 11:43:27 +0100

    [FEATURE] select frame

--- src/experiences/2-arts-graphiques/ts/interactive/paintingFrame.ts
+++ src/experiences/2-arts-graphiques/ts/interactive/paintingFrame.ts
@@ -1,11 +1,29 @@
-class Frame {
-  frames: HTMLDivElement[];
-  selectedFrame: HTMLDivElement;
+import { Frame } from "../frame";
+
+export default class Frames {
+  frames: Frame[];
+  selectedFrameId: string;
 
   constructor() {
-    const frame1 = document.querySelector<HTMLDivElement>("#frame-1");
-    const frame2 = document.querySelector<HTMLDivElement>("#frame-2");
-    const frame3 = document.querySelector<HTMLDivElement>("#frame-3");
+    const frame1 = new Frame("frame-1");
+    const frame2 = new Frame("frame-2");
+    const frame3 = new Frame("frame-3");
+
+    this.frames = [frame1, frame2, frame3];
+
+    frame1.addEventListener(() => this.selectFrame(frame1));
+    frame2.addEventListener(() => this.selectFrame(frame2));
+    frame3.addEventListener(() => this.selectFrame(frame3));
   }
-  selectFrame() {}
+  selectFrame(selectedFrame: Frame) {
+    this.selectedFrameId = selectedFrame.id;
+
+    this.frames.forEach((frame) => {
+      if (this.selectedFrameId === frame.id) {
+        frame.addBackground();
+      } else {
+        frame.removeBackground();
+      }
+    });
+  }
 }
--- src/experiences/2-arts-graphiques/ts/script.ts
+++ src/experiences/2-arts-graphiques/ts/script.ts
@@ -1,8 +1,9 @@
 import Transition from "./transition";
-
+import Frames from "./interactive/paintingFrame";
 import paint from "./interactive/paint";
 
 window.addEventListener("load", () => {
   // paint();
   new Transition();
+  new Frames();
 });
