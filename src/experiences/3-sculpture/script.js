2bc4ad77ff560ae985e55bcc4d9a9758835af807
Francia-bdz <francia.bidzimou@mmibordeaux.com>
2024-02-06 14:20:07 +0100

    feat : loading

--- src/experiences/3-sculpture/script.js
+++ src/experiences/3-sculpture/script.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import LoadPart from "./LoadPart";
 import IntroPopup from "./component/1-IntroPart/IntroPart";
 import RoughHewingPart from "./component/2-RoughHewingPart/RoughHewingPart";
 import DetailsPart from "./component/3-DetailsPart/DetailsPart";
@@ -16,6 +17,7 @@ const sizes = {
 //
 // INITIALIZATION
 //
+LoadPart();
 IntroPopup();
 const canvas = document.querySelector("canvas.webgl");
 
