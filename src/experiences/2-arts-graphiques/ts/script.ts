4188bc1ff996726083b4b2b0407fc0b27bf48109
Auréliane DOR <aureliane.dor@gmail.com>
2024-02-07 09:39:21 +0100

    add modal

--- src/experiences/2-arts-graphiques/ts/script.ts
+++ src/experiences/2-arts-graphiques/ts/script.ts
@@ -1,7 +1,19 @@
-import Transition from "./transition";
+import Transition from "./class/transition";
 import Frames from "./interactive/paintingFrame";
+import Modal from "./class/modal";
+
+export type ExperienceType = {
+  transition?: Transition;
+  frames?: Frames;
+  modal?: Modal;
+};
+const experience: ExperienceType = {};
 
 window.addEventListener("load", () => {
-  new Transition();
-  new Frames();
+  experience.transition = new Transition();
+  experience.frames = new Frames();
+  experience.modal = new Modal("modal", experience);
+  experience.modal.open();
 });
+
+export default experience;
