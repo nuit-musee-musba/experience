41ba00a4be7ba7d126e5d136bfd63a85adefd600
Auréliane DOR <aureliane.dor@gmail.com>
2024-02-07 09:42:23 +0100

    add class button

---
+++ src/experiences/2-arts-graphiques/ts/class/button.ts
@@ -0,0 +1,19 @@
+export default class Button {
+  button: HTMLButtonElement;
+  id: string;
+  click: any;
+
+  constructor(buttonName: string, onClick: any) {
+    const button = document.querySelector<HTMLButtonElement>(`.${buttonName}`);
+    if (!button) {
+      console.error(`unable to find .${buttonName} in the dom`);
+      return;
+    }
+
+    this.button = button;
+    this.id = buttonName;
+    this.click = () => onClick();
+
+    this.button.addEventListener("click", () => this.click());
+  }
+}
