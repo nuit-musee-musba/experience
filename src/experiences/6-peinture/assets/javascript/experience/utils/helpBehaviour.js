0137dc85bc5b506c0507ab52483aabd5b5c6eeaf
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2024-02-08 18:02:05 +0100

    commit de finnn

--- src/experiences/6-peinture/assets/javascript/experience/utils/helpBehaviour.js
+++ src/experiences/6-peinture/assets/javascript/experience/utils/helpBehaviour.js
@@ -5,27 +5,66 @@ import "shepherd.js/dist/css/shepherd.css";
 const tour = new Shepherd.Tour({
   useModalOverlay: true,
   defaultStepOptions: {
-    classes: "shadow-md bg-purple-dark",
+    classes: "shadow-md bg-purple-dark shepherd-button",
     scrollTo: true,
-    buttonsClasses: "btn-small-primary",
   },
 });
 
 tour.addStep({
   id: "example-step",
-  text: "This step is attached to the bottom of the <code>.example-css-selector</code> element.",
+  text: "Voici le livre. Vous pouvez cliquer sur les fruits à droite pour voir ici leurs significations !",
   attachTo: {
     element: ".book",
-    on: "bottom",
+    on: "right",
   },
   classes: "book",
   buttons: [
     {
-      text: "Next",
+      text: "Suivant",
+      defaultStepOptions: false,
+      classes: "btn-small-primary",
       action: tour.next,
     },
   ],
 });
 
+tour.addStep({
+  id: "example-step2",
+  text: "Voici la recette. Chaque ligne correspond à la description d'un fruit. En appuyant sur les fruits et en regardant le livre, trouvez les fruits dont vous avez besoin !",
+  attachTo: {
+    element: ".img-recipe",
+    on: "left",
+  },
+  classes: "img-recipe",
+  buttons: [
+    {
+      text: "Suivant",
+      defaultStepOptions: false,
+      classes: "btn-small-primary",
+      action: tour.next,
+    },
+  ],
+});
+
+tour.addStep({
+  id: "example-step3",
+  text: "Voici votre plan de travail. Quand vous avez repéré un fruit qui semble correspondre et à la description et à l'indication dans la recette, glissez-le ici.",
+  attachTo: {
+    element: ".composition",
+    on: "bottom",
+  },
+  classes: "composition",
+  buttons: [
+    {
+      text: "Terminer",
+      defaultStepOptions: false,
+      classes: "btn-small-primary",
+      action: tour.complete,
+    },
+  ],
+});
+
 // Démarrer le tour
-tour.start();
+export function tstart() {
+  tour.start();
+}
