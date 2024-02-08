f943c80731ed78d4c73a6b9c1b6650df0201ed7f
samuelabadie <samuel.abadiediez@gmail.com>
2024-02-08 10:16:49 +0100

    basic-text-transitions

--- src/js/scenes.js
+++ src/js/scenes.js
@@ -10,48 +10,80 @@ window.experience = window.experience || {};
 console.log(window.experience);
 const index = window.experience.index;
 
-// Add event listener to showButton
 showButton.addEventListener("click", () => {
-  // Calculate the target rotation based on 'index'
   if (!window.experience.isRotating) {
     window.experience.rotation = window.experience.index * (circle / parts);
-    console.log(
-      window.experience.rotation,
-      window.experience.index,
-      circle,
-      parts
-    );
-    // Loop through all elements with the class 'first-scene' and set display to 'none'
+
     firstSceneElements.forEach((element) => {
-      element.style.display = "none";
+      element.classList.add("transition-opacity");
+      element.style.opacity = "0";
     });
 
-    // Loop through all elements with the class 'second-scene' and set display to 'flex'
-    secondSceneElements.forEach((element) => {
-      element.style.display = "flex";
-    });
+    setTimeout(() => {
+      firstSceneElements.forEach((element) => {
+        element.style.display = "none";
+      });
+
+      secondSceneElements.forEach((element) => {
+        element.classList.add("transition-opacity");
+        element.style.display = "flex";
+        element.style.opacity = "0";
+      });
+
+      setTimeout(() => {
+        secondSceneElements.forEach((element) => {
+          element.style.opacity = "1";
+        });
+      }, 300);
+
+      firstSceneElements.forEach((element) => {
+        element.classList.remove("transition-opacity");
+      });
+      secondSceneElements.forEach((element) => {
+        element.classList.remove("transition-opacity");
+      });
+    }, 300);
   }
 });
 
-// Add event listener to backButton
 backButton.addEventListener("click", () => {
-  // Loop through all elements with the class 'second-scene' and set display to 'none'
   secondSceneElements.forEach((element) => {
-    element.style.display = "none";
+    element.classList.add("transition-opacity");
+    element.style.opacity = "0";
   });
 
-  // Loop through all elements with the class 'first-scene' and set display to 'flex'
-  firstSceneElements.forEach((element) => {
-    element.style.display = "flex";
-  });
+  setTimeout(() => {
+    secondSceneElements.forEach((element) => {
+      element.style.display = "none";
+    });
+
+    firstSceneElements.forEach((element) => {
+      element.style.display = "flex";
+      element.style.opacity = "0";
+    });
+
+    setTimeout(() => {
+      firstSceneElements.forEach((element) => {
+        element.style.opacity = "1";
+      });
+    }, 500);
+
+    secondSceneElements.forEach((element) => {
+      element.classList.remove("transition-opacity");
+    });
+    firstSceneElements.forEach((element) => {
+      element.classList.remove("transition-opacity");
+    });
+  }, 300);
 });
 
-// Render loop
 const tik = () => {
   requestAnimationFrame(tik);
-  window.experience.isRotating
-    ? (showButton.style.display = "none")
-    : (showButton.style.display = "block");
+  if (window.experience.isRotating) {
+    showButton.style.opacity = "0";
+  } else {
+    showButton.style.opacity = "1";
+  }
 };
 
 tik();
