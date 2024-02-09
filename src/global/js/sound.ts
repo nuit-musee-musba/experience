aa058ddb57775f25463eebc75aefe6eb80910bda
Arthaud <arthaudp33@gmail.com>
2024-02-09 12:46:57 +0100

    feat: enable autoplay in custom browser

--- src/global/js/sound.ts
+++ src/global/js/sound.ts
@@ -2,10 +2,16 @@ export const ambiantSound = (soundUrl: string) => {
   const audio = new Audio(soundUrl);
   audio.loop = true;
 
+  audio.play().catch((e) => {
+    console.log(
+      "[ambiant sound] autoplay not allowed, waiting user interaction to play ambiant sound"
+    );
+  });
+
   const methods = {
     playOnFirstInteraction: () => {
-      window.addEventListener("click", audio.play, { once: true });
-      window.addEventListener("dragstart", audio.play, { once: true });
+      window.addEventListener("click", () => audio.play(), { once: true });
+      window.addEventListener("tap", () => audio.play(), { once: true });
     },
   };
 
