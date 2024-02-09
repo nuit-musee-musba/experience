742d72b4e16df0be2fa7b26578a1515dec671b35
97Lucaas <lucas.duverneuil@mmibordeaux.com>
2024-02-09 10:07:08 +0100

    changes

--- src/experiences/6-peinture/assets/javascript/experience/utils/helpBehaviour.js
+++ src/experiences/6-peinture/assets/javascript/experience/utils/helpBehaviour.js
@@ -1,70 +1,77 @@
 import Shepherd from "shepherd.js";
 import "shepherd.js/dist/css/shepherd.css";
 
-// Initialisez Shepherd et créez le tour
-const tour = new Shepherd.Tour({
-  useModalOverlay: true,
-  defaultStepOptions: {
-    classes: "shadow-md bg-purple-dark shepherd-button",
-    scrollTo: true,
-  },
-});
-
-tour.addStep({
-  id: "example-step",
-  text: "Voici le livre. Vous pouvez cliquer sur les fruits à droite pour voir ici leurs significations !",
-  attachTo: {
-    element: ".book",
-    on: "right",
-  },
-  classes: "book",
-  buttons: [
-    {
-      text: "Suivant",
-      defaultStepOptions: false,
-      classes: "btn-small-primary",
-      action: tour.next,
+export function demarrerTour() {
+  let helpbtn = document.getElementById("helpbtn");
+  // Initialisez Shepherd et créez le tour
+  const tour = new Shepherd.Tour({
+    useModalOverlay: true,
+    defaultStepOptions: {
+      classes: "shadow-md bg-purple-dark shepherd-button",
+      scrollTo: true,
     },
-  ],
-});
+  });
 
-tour.addStep({
-  id: "example-step2",
-  text: "Voici la recette. Chaque ligne correspond à la description d'un fruit. En appuyant sur les fruits et en regardant le livre, trouvez les fruits dont vous avez besoin !",
-  attachTo: {
-    element: ".img-recipe",
-    on: "left",
-  },
-  classes: "img-recipe",
-  buttons: [
-    {
-      text: "Suivant",
-      defaultStepOptions: false,
-      classes: "btn-small-primary",
-      action: tour.next,
+  tour.addStep({
+    id: "example-step",
+    text: "Voici le livre. Vous pouvez cliquer sur les fruits à droite pour voir ici leurs significations !",
+    attachTo: {
+      element: ".book",
+      on: "right",
     },
-  ],
-});
+    classes: "book",
+    buttons: [
+      {
+        text: "Suivant",
+        defaultStepOptions: false,
+        classes: "btn-small-primary",
+        action: tour.next,
+      },
+    ],
+  });
 
-tour.addStep({
-  id: "example-step3",
-  text: "Voici votre plan de travail. Quand vous avez repéré un fruit qui semble correspondre et à la description et à l'indication dans la recette, glissez-le ici.",
-  attachTo: {
-    element: ".composition",
-    on: "bottom",
-  },
-  classes: "composition",
-  buttons: [
-    {
-      text: "Terminer",
-      defaultStepOptions: false,
-      classes: "btn-small-primary",
-      action: tour.complete,
+  tour.addStep({
+    id: "example-step2",
+    text: "Voici la recette. Chaque ligne correspond à la description d'un fruit. En appuyant sur les fruits et en regardant le livre, trouvez les fruits dont vous avez besoin !",
+    attachTo: {
+      element: ".img-recipe",
+      on: "left",
     },
-  ],
-});
+    classes: "img-recipe",
+    buttons: [
+      {
+        text: "Suivant",
+        defaultStepOptions: false,
+        classes: "btn-small-primary",
+        action: tour.next,
+      },
+    ],
+  });
 
-// Démarrer le tour
-export function tstart() {
+  tour.addStep({
+    id: "example-step3",
+    text: "Voici votre plan de travail. Quand vous avez repéré un fruit qui semble correspondre et à la description et à l'indication dans la recette, glissez-le ici.",
+    attachTo: {
+      element: ".composition",
+      on: "bottom",
+    },
+    classes: "composition",
+    buttons: [
+      {
+        text: "Terminer",
+        defaultStepOptions: false,
+        classes: "btn-small-primary",
+        action: tour.complete,
+      },
+    ],
+  });
+
+  helpbtn.style.display = "none";
+
   tour.start();
+
+  tour.on("complete", function () {
+    // Fonction à exécuter une fois le tour complété
+    helpbtn.style.display = "block";
+  });
 }
