8a99e929a7b651b28d4d1492fbccb1b979d090f4
Olivia206 <olivia.simonet206@gmail.com>
2024-02-09 15:09:48 +0100

    gp6: iddle animation and wording

--- src/experiences/6-peinture/assets/javascript/experience/index.js
+++ src/experiences/6-peinture/assets/javascript/experience/index.js
@@ -3,3 +3,10 @@ import "./components/Book";
 import "./components/dialog";
 import "./components/endingCinematic";
 import "./components/tab";
+
+import { playAnimation } from "./utils/playAnimation.js";
+
+const gameScene = document.querySelector('.page__game');
+if (gameScene) {
+    playAnimation("animJeffIddle");
+}
